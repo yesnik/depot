d2a046195f1ad1792afd35dc7217b65f1a523f24
Nikita Nyatin <yesnik@yandex.ru>
2015-01-15 16:34:59 +0500

    Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 # encoding: utf-8
 
 class Product < ActiveRecord::Base
-  validates :title, :description, :image_url, presence: true
+  validates :title, :description, :image_url, presence: {message: 'не может быть пустым'}
+  validates :title, length: { minimum: 4, message: 'должен быть минимум 4 символа' }
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: { with: /\.(gif|jpg|png)\z/i,
