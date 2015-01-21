9f0e1c543d8981904465a9b504322ff1877cce33
Nikita Nyatin <yesnik@yandex.ru>
2015-01-21 10:15:51 +0500

    Edit cart page

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,8 +31,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         session[:counter] = 0
-        format.html { redirect_to @line_item.cart, 
-          notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
