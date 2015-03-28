0f6676a7c4abd1270dab861b105a9df62a2bc858
Nikita Nyatin <yesnik@yandex.ru>
2015-03-28 09:57:32 +0500

    Add highlight to cart items, fix templates

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,7 +32,7 @@ class LineItemsController < ApplicationController
       if @line_item.save
         session[:counter] = 0
         format.html { redirect_to store_url }
-        format.js
+        format.js { @current_item = @line_item }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
