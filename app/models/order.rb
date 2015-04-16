2cde55fbd4fec1c4daec74eaa7009b73511e0fea
Nikita Nyatin <yesnik@yandex.ru>
2015-04-16 11:04:49 +0500

    Add tests

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,6 @@
 class Order < ActiveRecord::Base
   PAYMENT_TYPES = [ "check", "credit_card", "cash" ]
+
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
 end
--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -8,7 +8,7 @@ class StoreControllerTest < ActionController::TestCase
     assert_select '#columns #side a', minimum: 2
     assert_select '#main .entry', Product.all.size
     assert_select 'h3', 'Programming Ruby 1.9'
-    assert_select '.datetime_current', /Сейчас.+/
+    assert_select '.datetime_current', /Сейча.+/
     assert_select '.price', /[,\d]+\.\d\d\sруб/
   end
 
