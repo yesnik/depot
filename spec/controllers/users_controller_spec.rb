88250010411039446ec6fc3f26585a974bce01ef
Nikita Nyatin <yesnik@yandex.ru>
2015-04-22 14:55:09 +0500

    Fix rspec tests for User

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -24,11 +24,11 @@ RSpec.describe UsersController, :type => :controller do
   # User. As you add validations to User, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    {name: 'kenny', password: '123', password_confirmation: '123'}
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    {name: '', password: '123', password_confirmation: '123'}
   }
 
   # This should return the minimal set of values that should be in the session
@@ -81,9 +81,9 @@ RSpec.describe UsersController, :type => :controller do
         expect(assigns(:user)).to be_persisted
       end
 
-      it "redirects to the created user" do
+      it "redirects to the users lits" do
         post :create, {:user => valid_attributes}, valid_session
-        expect(response).to redirect_to(User.last)
+        expect(response).to redirect_to(users_url)
       end
     end
 
@@ -103,14 +103,14 @@ RSpec.describe UsersController, :type => :controller do
   describe "PUT update" do
     describe "with valid params" do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        {name: 'kenny2', password: '123', password_confirmation: '123'}
       }
 
       it "updates the requested user" do
         user = User.create! valid_attributes
         put :update, {:id => user.to_param, :user => new_attributes}, valid_session
         user.reload
-        skip("Add assertions for updated state")
+        expect(user.name).to eq 'kenny2'
       end
 
       it "assigns the requested user as @user" do
@@ -122,7 +122,7 @@ RSpec.describe UsersController, :type => :controller do
       it "redirects to the user" do
         user = User.create! valid_attributes
         put :update, {:id => user.to_param, :user => valid_attributes}, valid_session
-        expect(response).to redirect_to(user)
+        expect(response).to redirect_to(users_url)
       end
     end
 
