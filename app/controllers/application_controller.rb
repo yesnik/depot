5f5601ef857fc5f8ded9dfebd110d577a63dc93d
Nikita Nyatin <yesnik@yandex.ru>
2015-05-19 14:20:41 +0500

    Add translations and en locale support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,26 @@
 # encoding: utf-8
 class ApplicationController < ActionController::Base
   before_action :authorize
+  before_filter :set_locale
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Метод нужен для поддержки генерации URL-ов, соответствующих локали,
+  # хелпером link_to
+  def default_url_options(options={})
+    logger.debug "default_url_options is passed options: #{options.inspect}\n"
+    { :locale => I18n.locale }
+  end
+
+  protected
+
+  def authorize
+    unless User.find_by(id: session[:user_id])
+      redirect_to login_url, notice: t('app.controllers.session.messages.authorize_please')
+    end
+  end
 
   private
 
@@ -16,12 +33,8 @@ class ApplicationController < ActionController::Base
     cart
   end
 
-  protected
-
-  def authorize
-    unless User.find_by(id: session[:user_id])
-      redirect_to login_url, notice: t('app.controllers.session.messages.authorize_please')
-    end
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
 
