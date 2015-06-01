fc9a5073265e8a0eb96e8e30fa67b1482863c4eb
Nikita Nyatin <yesnik@yandex.ru>
2015-06-01 17:23:30 +0500

    Add language menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,9 @@ module ApplicationHelper
     end
     content_tag('div', attributes, &block)
   end
+
+  # Define if this page is current
+  def cp(path)
+    "current" if current_page?(path)
+  end
 end
