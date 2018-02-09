561d554f5221669c72af9a102b932fcea4f86ee7
Nikita Nyatin <yesnik@yandex.ru>
2018-02-09 18:24:03 +0500

    Add seeds and images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.delete_all
+
+Product.create(
+  title: 'Simple PHP',
+	description:
+	%{<p>
+	This is a nice book about PHP
+	</p>},
+	image_url: 'php.jpg',
+	price: 26.00
+)
+
+Product.create(
+  title: 'Python',
+	description:
+	%{<p>
+	This is a nice book about Python.
+	},
+	image_url: 'python.jpg',
+	price: 16.00
+)
+
+Product.create(
+  title: 'Ruby is our friend',
+	description:
+	%{<p>
+	This is a great book about Ruby programming language.
+	We are sure that you'll like it!
+	</p>},
+	image_url: 'ruby.jpg',
+	price: 31.00
+)
