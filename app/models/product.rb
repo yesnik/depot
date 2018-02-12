f0689fa7a739c9874f16997f207460022c9c54a6
Nikita Nyatin <yesnik@yandex.ru>
2018-02-12 10:57:32 +0500

    Add numericality validation to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, :price, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Product, type: :model do
   it { is_expected.to validate_presence_of :description }
   it { is_expected.to validate_presence_of :image_url }
   it { is_expected.to validate_presence_of :price }
+
+  it do
+  	is_expected.to validate_numericality_of(:price),
+	    greater_than_or_equal_to: 0.01
+  end
 end
