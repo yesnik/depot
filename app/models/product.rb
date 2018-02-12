9e6df6cee7382c851e553d8ea1b20b149fe12340
Nikita Nyatin <yesnik@yandex.ru>
2018-02-12 17:29:46 +0500

    Add custom error message to title

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   validates :description, :image_url, :price, presence: true
-  validates :title, length: {minimum: 5}
+  validates :title,
+  			length: {minimum: 5,
+  					 message: 'Title length should be minimum 5 characters'}
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: {case_sensitive: false}
   validates :image_url,
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  it { is_expected.to validate_length_of(:title).is_at_least(5) }
+  it do
+  	is_expected.to validate_length_of(:title).is_at_least(5).
+  		with_message('Title length should be minimum 5 characters')
+  end
+
   it { is_expected.to validate_presence_of :description }
   it { is_expected.to validate_presence_of :image_url }
   it { is_expected.to validate_presence_of :price }
