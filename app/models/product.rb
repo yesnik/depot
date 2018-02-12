9bbdfe02769d7a47b28fc36f0ed669d51ed1f5fc
Nikita Nyatin <yesnik@yandex.ru>
2018-02-12 11:26:28 +0500

    Add validation for image_url

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ApplicationRecord
   validates :title, :description, :image_url, :price, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: {case_sensitive: false}
+  validates :image_url,
+			format: {with: %r{\.(gif|jpg|png)\Z}i,
+					 message: 'Only .png, .jpg, .gif images are allowed'}
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Product, type: :model do
   end
 
   it { is_expected.to validate_uniqueness_of(:title).case_insensitive }
+
+  it { is_expected.to allow_value('hi.jpg').for(:image_url) }
+  it { is_expected.to allow_value('hi.png').for(:image_url) }
+  it { is_expected.to allow_value('hi.gif').for(:image_url) }
+  it do
+  	is_expected.not_to allow_value('hi.mp4').for(:image_url).
+      with_message('Only .png, .jpg, .gif images are allowed')
+  end
 end
