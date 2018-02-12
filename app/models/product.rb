6390b5f9936de867587d64c4e796d1162549a3f9
Nikita Nyatin <yesnik@yandex.ru>
2018-02-12 17:24:08 +0500

    Add validation to title length

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
-  validates :title, :description, :image_url, :price, presence: true
+  validates :description, :image_url, :price, presence: true
+  validates :title, length: {minimum: 5}
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: {case_sensitive: false}
   validates :image_url,
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  it { is_expected.to validate_presence_of :title }
+  it { is_expected.to validate_length_of(:title).is_at_least(5) }
   it { is_expected.to validate_presence_of :description }
   it { is_expected.to validate_presence_of :image_url }
   it { is_expected.to validate_presence_of :price }
@@ -16,6 +16,7 @@ RSpec.describe Product, type: :model do
   it { is_expected.to allow_value('hi.jpg').for(:image_url) }
   it { is_expected.to allow_value('hi.png').for(:image_url) }
   it { is_expected.to allow_value('hi.gif').for(:image_url) }
+  it { is_expected.to allow_value('hi.GiF').for(:image_url) }
   it do
   	is_expected.not_to allow_value('hi.mp4').for(:image_url).
       with_message('Only .png, .jpg, .gif images are allowed')
