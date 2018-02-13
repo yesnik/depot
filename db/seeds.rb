8a55b28fdc2ed1803147aee96bd7a67c73e91cff
Nikita Nyatin <yesnik@yandex.ru>
2018-02-13 09:39:20 +0500

    Change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,8 +21,20 @@ Product.create(
 Product.create(
   title: 'Python',
 	description:
-	%{<p>
-	This is a nice book about Python.
+	%{
+		<p>Python Crash Course is a fast-paced, thorough introduction 
+		to programming with Python that will have you writing programs, 
+		solving problems, and making things that work in no time.</p>
+
+		<p>In the first half of the book, you’ll learn about basic programming 
+		concepts, such as lists, dictionaries, classes, and loops, and practice 
+		writing clean and readable code with exercises for each topic. 
+		You’ll also learn how to make your programs interactive and how 
+		to test your code safely before adding it to a project. 
+		In the second half of the book, you’ll put your new knowledge into 
+		practice with three substantial projects: a Space Invaders-inspired 
+		arcade game, data visualizations with Python’s super-handy libraries, 
+		and a simple web app you can deploy online.</p>
 	},
 	image_url: 'python.jpg',
 	price: 16.00
@@ -31,10 +43,19 @@ Product.create(
 Product.create(
   title: 'Ruby is our friend',
 	description:
-	%{<p>
-	This is a great book about Ruby programming language.
-	We are sure that you'll like it!
-	</p>},
+	%{
+		<p>This book is a tutorial and reference for the <i>Ruby programming language</i>. 
+		Use Ruby, and you'll write better code, be more productive, 
+		and enjoy programming more.</p>
+
+		<p>These are bold claims, but we think that after reading this book you'll 
+		agree with them. And we have the experience to back up this belief.</p>
+
+		<p>As <i>Pragmatic Programmers</i> we've tried many, many languages in our 
+		search for tools to make our lives easier, for tools to help us do 
+		our jobs better. Until now, though, we'd always been frustrated 
+		by the languages we were using.</p>
+	},
 	image_url: 'ruby.jpg',
 	price: 31.00
 )
