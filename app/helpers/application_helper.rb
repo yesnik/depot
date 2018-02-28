b539e01013946a9b5e213d282e939aff58bc6d17
Nikita Nyatin <yesnik@yandex.ru>
2018-02-28 10:21:22 +0500

    Hide empty cart

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def hidden_div_if(hide, html_options={}, &block)
+    if hide
+      html_options[:class] = 'hide'
+    end
+
+    tag.div html_options do
+      block.call
+    end
+  end
 end
