8684531a8b1d55dcfd6ba81d966d1a5e6ef3c0b9
Nikita Nyatin <yesnik@yandex.ru>
2018-03-07 16:49:45 +0500

    Change pay type migration

--- db/migrate/20180307105042_create_pay_types.rb
+++ db/migrate/20180307105042_create_pay_types.rb
@@ -1,9 +1,23 @@
 class CreatePayTypes < ActiveRecord::Migration[5.1]
-  def change
+  def up
     create_table :pay_types do |t|
       t.string :title
 
       t.timestamps
     end
+
+    populate_pay_types
+  end
+
+  def down
+    drop_table :pay_types
+  end
+
+  protected
+
+  def populate_pay_types
+    PayType.create(title: 'Check')
+    PayType.create(title: 'Credit card')
+    PayType.create(title: 'Purchase order')
   end
 end
