06f0cb3bd8a2cc1d3ae94bd18aadb2c1049383a3
Nikita Nyatin <yesnik@yandex.ru>
2018-03-19 17:33:19 +0500

    Add name validation for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  
   has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_uniqueness_of :name }
+  end
 end
