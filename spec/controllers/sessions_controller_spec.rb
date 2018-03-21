0c47d8b5a3dce559b1fe91c6a3c8d09ba17bb8c3
Nikita Nyatin <yesnik@yandex.ru>
2018-03-21 16:14:43 +0500

    Repair tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -11,15 +11,15 @@ RSpec.describe SessionsController, type: :controller do
 
   describe "GET #create" do
     it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+      post :create
+      expect(response).to have_http_status(302)
     end
   end
 
   describe "GET #destroy" do
     it "returns http success" do
       get :destroy
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(302)
     end
   end
 
