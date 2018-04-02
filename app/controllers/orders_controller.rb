3c7ee6b8c736f5d7329bd41a2017c9c7560aa313
Nikita Nyatin <yesnik@yandex.ru>
2018-04-02 16:38:12 +0500

    Add language switcher

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -39,8 +39,7 @@ class OrdersController < ApplicationController
         session[:cart_id] = nil
         OrderMailer.received(@order).deliver_later
         
-        format.html { redirect_to store_index_url, notice:
-          'Thank you for your order.' }
+        format.html { redirect_to store_index_url, notice: I18n.t('.thanks') }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new }
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -13,7 +13,11 @@ class StoreController < ApplicationController
       return render plain: visits_counter
     end
 
-  	@products = Product.order(:title)
+    if params[:set_locale]
+      redirect_to store_index_url(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
+    end
   end
 
   protected
