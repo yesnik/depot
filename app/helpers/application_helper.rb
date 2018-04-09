5e31f6103c61a11c8d9d8d21cd19294aad49d76f
Nikita Nyatin <yesnik@yandex.ru>
2018-04-09 12:22:45 +0500

    Add localized price

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  EXCHANGE_RATES = {
+    ru: 58.2
+  }
+
   def hidden_div_if(hide, html_options={}, &block)
     if hide
       html_options[:class] = 'hide'
@@ -7,5 +11,10 @@ module ApplicationHelper
     tag.div html_options do
       block.call
     end
+  end
+
+  def localized_price(price)
+    price = price * EXCHANGE_RATES[I18n.locale.to_sym] if I18n.locale != :en
+    number_to_currency(price, delimiter: ' ')
   end
 end
