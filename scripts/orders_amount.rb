f95324fbc7f86dd62c9201bf0c1ee94b5144df2f
Nikita Nyatin <yesnik@yandex.ru>
2018-04-24 18:01:42 +0500

    Add easy way to get orders count

--- scripts/orders_amount.rb
+++ scripts/orders_amount.rb
@@ -1,13 +1,3 @@
-require 'active_record'
-
-ActiveRecord::Base.establish_connection(
-  adapter: 'postgresql',
-  database: 'depot_development',
-  user: 'depot',
-  password: '123',
-  host: 'localhost'
-)
-
-class Order < ActiveRecord::Base; end
+require_relative '../config/environment.rb'
 
 p Order.count
