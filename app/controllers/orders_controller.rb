09e4263634b3cb6e54783f077dad49423a89a404
Nikita Nyatin <yesnik@yandex.ru>
2018-04-26 17:31:55 +0500

    Add pagination to orders index page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,7 +10,7 @@ class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.json
   def index
-    @orders = Order.all
+    @orders = Order.order('created_at desc').page(params[:page])
   end
 
   # GET /orders/1
